60c2bd97e0ba14ae8bf122ac19c5f84f3e79f0d5
Chester-yan <53684681+Chester-yan@users.noreply.github.com>
2020-02-17 13:09:01 +0800

    mac-提交
    表單驗證實現，開始寫CSS裝飾

--- Game_Platform/index/froms.py
+++ Game_Platform/index/froms.py
@@ -1,19 +1,62 @@
 from django import forms
+from django.forms import MultiWidget
+from django.core.exceptions import ValidationError 
+from django.contrib.postgres import validators
 
 from .models import *
 
 import datetime
 
+def validator_uname(value):
+    try:
+        User.objects.get(uname=value)
+
+        raise ValidationError('%s用戶名稱已存在' % value)
+    except User.DoesNotExist:
+        pass
+
+
 
 class RegisterForm(forms.ModelForm):
+    nowyear = datetime.datetime.now().year
+
+    uname = forms.CharField(label='使用者名稱',
+        required = True,
+        min_length = 5,
+        max_length = 30,
+        error_messages = {
+            'required':'這裡必須填寫',
+            'min_length':'用戶名至少5字數',
+            'max_length':'用戶名最多30字數'
+            },
+        validators = [validator_uname],
+        
+        widget = forms.TextInput(
+            attrs = {'id':'info_input','placeholder':'設定使用者名稱'}
+        )
+
+        )
+    cpwd = forms.CharField(
+        label='確認密碼',
+        widget=forms.PasswordInput(
+        attrs={'id':'info_input','placeholder':'請再輸入一次密碼'}
+        ),
+        )
+
+    ubd = forms.DateField(label='生日',
+        widget=forms.SelectDateWidget(
+            years=range(nowyear, 1930, -1)
+            ))
+
     # 通過內部類Meta表示關聯的訊息
     class Meta:
         # 指定關聯的model
         model = User
+        # exclude = ('id',)
 
         # 指定要顯示的控件
-        fields = ['uname','upwd','ubd','uemail']
-
+        fields = ['uname','upwd','cpwd','ubd','uemail']
+        
         # 指定控件對應的label
         labels = {
             'uname':'使用者名稱',
@@ -23,32 +66,37 @@ class RegisterForm(forms.ModelForm):
             'uemail':'信箱'
         }
 
+        # errMsg = {'required':'這裡必須填寫'}
+
+
         # 指定每個控件對應的小部件，並給出其他屬性
-        nowyear = datetime.datetime.now().year
+        
         widgets = { 
             'uname':forms.TextInput(attrs= {
-                'class':'info_input',
+                'id':'info_input',
                 'placeholder':'設定使用者名稱'
             }),
 
             'upwd':forms.PasswordInput(attrs={
-                'class':'info_input',
+                'id':'info_input',
                 'placeholder':'設定密碼',
             }),
 
-            'cpwd':forms.PasswordInput(attrs={
-                'class':'info_input',
-                'placeholder':'請再輸入一次密碼',
-            }),
-
-            'ubd':forms.SelectDateWidget(years=range(1900,nowyear), 
-            attrs={
-                'class':'info_input',
-            }),
-
             'uemail':forms.EmailInput(attrs={
-                'class':'info_input',
+                'id':'info_input',
                 'placeholder':'請輸入信箱',
             }),                        
 
         }
+
+    def clean_cpwd(self):
+        upwd = self.cleaned_data.get('upwd')
+        cpwd = self.cleaned_data.get('cpwd')
+        if upwd and cpwd and upwd != cpwd:
+            raise forms.ValidationError('密碼不相符')
+        return cpwd
+        
+
+
+
+
--- Game_Platform/index/views.py
+++ Game_Platform/index/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from django.views.decorators.clickjacking import xframe_options_sameorigin
+from django.contrib.auth import authenticate,login,logout
 
 
 
@@ -26,45 +27,75 @@ def index_views(request):
 
 @xframe_options_sameorigin
 def register_views(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('註冊成功')
+#             # return render(request,'02-register.html',locals())
+
+#     else:
+#         form = RegisterForm()
+#         # errorMsg = field.error_messages
+#         # return render(request,'02-register.html',locals())
+
+#     return render(request,'02-register.html',locals())
+
+
     # 判斷get或post請求，得到用戶的請求意圖
     if request.method == 'GET':
         form = RegisterForm()
 
         return render(request,'02-register.html',locals())
+    
+    else:
+        form = RegisterForm(request.POST)
 
+        if form.is_valid():
+            user = User(**form.cleaned_data)
+            # 數據庫配置成功並且實體類映射成表之後，該操作可以實現
+            user.save() 
+        else:
 
+            # errorMsg = form.errors
 
-    #     return render(request,'02-register.html')
-    # else:
-    #     # 先驗證帳號與信箱是否已存在於數據庫
-    #     # 驗證帳號名稱
-    #     uname = request.POST['uname']
-    #     users = User.objects.filter(uname=uname)
-    #     if users:
-    #         # 帳號已存在
-    #         errMsg = '帳號名稱已存在'
-    #         return render(request,'02-register.html',locals())
-    #     upwd = request.POST['upwd']
+            # for field in errorMsg:
+            #     print(field)
 
-    #     ubd = request.POST['ubd']
+            return render(request,'02-register.html',locals())
 
-    #     uemail = request.POST['uemail']
-    #     uemails = User.objects.filter(uemail=uemail)
-    #     if uemails:
-    #         # 信箱已存在
-    #         errMsg = '信箱已註冊過'
-    #         return render(request,'02-register.html',locals())
+    return render(request,'02-register.html')
+
+        # # 先驗證帳號與信箱是否已存在於數據庫
+        # # 驗證帳號名稱
+        # uname = request.POST['uname']
+        # users = User.objects.filter(uname=uname)
+        # if users:
+        #     # 帳號已存在
+        #     errMsg = '帳號名稱已存在'
+        #     return render(request,'02-register.html',locals())
+        # upwd = request.POST['upwd']
+
+        # ubd = request.POST['ubd']
+
+        # uemail = request.POST['uemail']
+        # uemails = User.objects.filter(uemail=uemail)
+        # if uemails:
+        #     # 信箱已存在
+        #     errMsg = '信箱已註冊過'
+        #     return render(request,'02-register.html',locals())
         
-    #     # 接收數據插入到數據庫中
+        # # 接收數據插入到數據庫中
 
     
-    #     user = User()
-    #     user.uname = uname
-    #     user.upwd = upwd
-    #     user.ubd = ubd
-    #     user.uemail = uemail
-    #     user.save()
-    #     return HttpResponse('註冊成功')
+        # user = User()
+        # user.uname = uname
+        # user.upwd = upwd
+        # user.ubd = ubd
+        # user.uemail = uemail
+        # user.save()
+        # return HttpResponse('註冊成功')
 
 
 
