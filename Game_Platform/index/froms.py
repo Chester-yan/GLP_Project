07bb21e3293d34c53a6fe39a35a8a1ce48111d7b
Chester-yan <53684681+Chester-yan@users.noreply.github.com>
2020-02-18 00:34:16 +0800

    mac-提交
    本次新增內容：
    1.登入頁面、forms.py功能、view.py功能基本建構完成
    
    下一階段：
    1.完整登入失敗、成功提示
    2.無此帳號提示
    3.密碼錯誤提示

--- Game_Platform/index/froms.py
+++ Game_Platform/index/froms.py
@@ -81,18 +81,6 @@ class RegisterForm(forms.ModelForm):
         # 指定關聯的model
         model = User
 
-        # # 遮蔽不顯示的控件
-        # exclude = [
-        #     'GENDER_CHOICES',
-        #     'ugender',
-        #     'uphoto',
-        #     'uintro',
-        #     'ucredit',
-        #     'ufriend',
-        #     'usubs',
-        #     'isActive'
-        # ]
-
         # 指定要顯示的控件
         fields = ['uname','upwd','cpwd','ubd','uemail']
         
@@ -105,53 +93,31 @@ class RegisterForm(forms.ModelForm):
             'uemail':'信箱'
             }
 
-        # errMsg = {'required':'這裡必須填寫'}
+
+class LoginForm(forms.ModelForm):
+    """Form definition for Login."""
 
 
-        # # 指定每個控件對應的小部件，並給出其他屬性
-        # widgets = { 
-        #     'uname':forms.TextInput(attrs= {
-        #         'id':'info_input',
-        #         'placeholder':'設定使用者名稱'
-        #     }),
+    class Meta:
+        """Meta definition for Loginform."""
 
-        #     'upwd':forms.PasswordInput(attrs={
-        #         'id':'info_input',
-        #         'placeholder':'設定密碼',
-        #     }),
+        model = User
 
-        #     'uemail':forms.EmailInput(attrs={
-        #         'id':'info_input',
-        #         'placeholder':'請輸入信箱',
-        #     }),                        
+        fields = ('uname','upwd')
 
-        # }
+        labels = {
+            'uname':'使用者名稱',
+            'upwd':'密碼',
+            }
 
-    # def clean_cpwd(self):
-    #     upwd = self.cleaned_data.get('upwd')
-    #     cpwd = self.cleaned_data.get('cpwd')
-    #     if upwd and cpwd and upwd != cpwd:
-
-    #         raise forms.ValidationError('密碼不相符')
-    #     else:
-
-    #         return cpwd
-    
-    # 定义全局的钩子，用来校验密码和确认密码字段是否相同，执行全局钩子的时候，
-    # cleaned_data里面肯定是有了通过前面验证的所有数据
-    
-
-
-    # # 定义全局的钩子，用来校验密码和确认密码字段是否相同，执行全局钩子的时候，cleaned_data里面肯定是有了通过前面验证的所有数据
-    # def clean(self):
-    #     password_value = self.cleaned_data.get('password')
-    #     re_password_value = self.cleaned_data.get('re_password')
-    #     if password_value == re_password_value:
-    #         return self.cleaned_data #全局钩子要返回所有的数据
-    #     else:
-    #         self.add_error('re_password', '两次密码不一致') #在re_password这个字段的错误列表中加上一个错误，并且clean_data里面会自动清除这个re_password的值，所以打印clean_data的时候会看不到它
-    #         raise ValidationError('两次密码不一致')
-        
+        widgets = {
+            'uname':forms.TextInput(attrs = {
+                'id':'info_input','placeholder':'請輸入使用者名稱'
+            }),
+            'upwd':forms.PasswordInput(attrs={
+                'id':'info_input','placeholder':'請輸入帳號密碼'
+            }),
+        }        
 
 
 
--- Game_Platform/index/views.py
+++ Game_Platform/index/views.py
@@ -46,18 +46,51 @@ def register_views(request):
         else:
             return render(request,'02-register.html',locals())
 
-        
-
-
-
-
-
-
-
-
-
 
 def login_views(request):
-    return render(request, '03-login.html')
+    # 判斷 get 請求還是 post 請求
+    if request.method == 'GET':
+        # get 請求 - 判斷session,判斷cookie,登入頁
+        # 判斷 session中是否有登入訊息
+        if 'uname' in request.session and 'upwd' in request.session:
+            # 有登入訊息保存在 session
+            return HttpResponse('您已經登入成功了')
+        else:
+            # 沒有登入訊息保存在session，繼續判斷cookies中是否有登入訊息
+            if 'uname' in request.COOKIES and 'upwd' in request.COOKIES:
+                # cookies中有登入訊息 - 曾經記住過密碼
+                # 將cookies中的訊息取出來保存進session，再返回到首頁
+                uname = request.COOKIES['uname']
+                upwd = request.COOKIES['upwd']
+                request.session['uname'] = uname
+                request.session['upwd'] = upwd
+                return HttpResponse('已登入成功')
+            else:
+                # cookies中沒有登入訊息 - 去往登入頁
+                form = LoginForm()
+                return render(request,'03-login.html',locals())
 
+    else:
+        # post請求 - 實現登入操作
+        # 獲取手機號與密碼
+        uname = request.POST['uname']
+        upwd = request.POST['upwd']
+        # 判斷手機號與密碼是否存在(登入是否成功)
+        users = User.objects.filter(uname=uname,upwd=upwd)
+        if users:
+            # 登入成功 : 先存進session
+            request.session['uname'] = users[0].id
+            request.session['upwd'] = upwd
+            # 聲明回應對象 : 回應一句話'登入成功'
+            resp = HttpResponse('登入成功')
+            # 判斷是否要存進cookies
+            if 'isSaved' in request.POST:
+                expire = 60*60*24*90
+                resp.set_cookie('uname',users[0].id,expire)
+                resp.set_cookie('upwd',upwd,expire)
+            return HttpResponse('登入成功',resp) 
+        else:
+            # 登入失敗
+            form = LoginForm()
+            return render(request,'03-login.html',locals())
     
