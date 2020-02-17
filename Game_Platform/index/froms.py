ba86ae77c52813fc564eed3e2d95b731cd410da4
Chester-yan <53684681+Chester-yan@users.noreply.github.com>
2020-02-17 23:33:05 +0800

    mac-提交
    本次修改：
    1.註冊驗證修正cpwd帶入數據庫問題
    2.完成註冊表單信箱重複性驗證功能
    
    之後有時間在解決的議題：
    1.試著從forms.py解決cpwd驗證重複性後不帶值回數據庫。
    
    2.細化註冊資料輸入條件及功能
    3.完整表單輸入提示
    
    以上

--- Game_Platform/index/froms.py
+++ Game_Platform/index/froms.py
@@ -11,18 +11,23 @@ def validator_uname(value):
     try:
         User.objects.get(uname=value)
 
-        raise ValidationError('%s用戶名稱已存在' % value)
+        raise ValidationError('%s用戶名稱已註冊' % value)
+    except User.DoesNotExist:
+        print('此用戶名稱可使用')
+
+def validator_uemail(value):
+    try:
+        User.objects.get(uemail=value)
+        raise ValidationError('此信箱已註冊過')
     except User.DoesNotExist:
         pass
-
-
 
 class RegisterForm(forms.ModelForm):
     nowyear = datetime.datetime.now().year
 
     uname = forms.CharField(label='使用者名稱',
         required = True,
-        min_length = 5,
+        min_length = 4,
         max_length = 30,
         error_messages = {
             'required':'這裡必須填寫',
@@ -33,26 +38,60 @@ class RegisterForm(forms.ModelForm):
         
         widget = forms.TextInput(
             attrs = {'id':'info_input','placeholder':'設定使用者名稱'}
-        )
+        ))
 
-        )
-    cpwd = forms.CharField(
-        label='確認密碼',
-        widget=forms.PasswordInput(
-        attrs={'id':'info_input','placeholder':'請再輸入一次密碼'}
+    upwd = forms.CharField(
+        label = '密碼',
+        widget = forms.PasswordInput(
+        attrs = {'id':'info_input','placeholder':'設定密碼'}
         ),
         )
 
-    ubd = forms.DateField(label='生日',
-        widget=forms.SelectDateWidget(
-            years=range(nowyear, 1930, -1)
-            ))
+
+    cpwd = forms.CharField(
+        label = '確認密碼',
+        widget = forms.PasswordInput(
+        attrs = {'id':'info_input','placeholder':'請再輸入一次密碼'}
+        ),
+        )
+
+    ubd = forms.DateField(
+        label='生日',
+        widget=forms.SelectDateWidget(years=range(nowyear, 1930, -1),
+        )
+        )
+
+    uemail = forms.CharField(
+        label = '信箱',
+        widget = forms.EmailInput(
+        attrs = {'id':'info_input','placeholder':'請輸入信箱'}
+        ),
+        validators = [validator_uemail],
+        )     
+
+    def clean_cpwd(self):
+        upwd = self.cleaned_data.get('upwd')
+        cpwd = self.cleaned_data.get('cpwd')
+        if upwd and cpwd and upwd != cpwd:
+            raise forms.ValidationError('密碼不相符')
+        return cpwd
 
     # 通過內部類Meta表示關聯的訊息
     class Meta:
         # 指定關聯的model
         model = User
-        # exclude = ('id',)
+
+        # # 遮蔽不顯示的控件
+        # exclude = [
+        #     'GENDER_CHOICES',
+        #     'ugender',
+        #     'uphoto',
+        #     'uintro',
+        #     'ucredit',
+        #     'ufriend',
+        #     'usubs',
+        #     'isActive'
+        # ]
 
         # 指定要顯示的控件
         fields = ['uname','upwd','cpwd','ubd','uemail']
@@ -64,37 +103,54 @@ class RegisterForm(forms.ModelForm):
             'cpwd':'確認密碼',
             'ubd':'生日',
             'uemail':'信箱'
-        }
+            }
 
         # errMsg = {'required':'這裡必須填寫'}
 
 
-        # 指定每個控件對應的小部件，並給出其他屬性
-        
-        widgets = { 
-            'uname':forms.TextInput(attrs= {
-                'id':'info_input',
-                'placeholder':'設定使用者名稱'
-            }),
+        # # 指定每個控件對應的小部件，並給出其他屬性
+        # widgets = { 
+        #     'uname':forms.TextInput(attrs= {
+        #         'id':'info_input',
+        #         'placeholder':'設定使用者名稱'
+        #     }),
 
-            'upwd':forms.PasswordInput(attrs={
-                'id':'info_input',
-                'placeholder':'設定密碼',
-            }),
+        #     'upwd':forms.PasswordInput(attrs={
+        #         'id':'info_input',
+        #         'placeholder':'設定密碼',
+        #     }),
 
-            'uemail':forms.EmailInput(attrs={
-                'id':'info_input',
-                'placeholder':'請輸入信箱',
-            }),                        
+        #     'uemail':forms.EmailInput(attrs={
+        #         'id':'info_input',
+        #         'placeholder':'請輸入信箱',
+        #     }),                        
 
-        }
+        # }
 
-    def clean_cpwd(self):
-        upwd = self.cleaned_data.get('upwd')
-        cpwd = self.cleaned_data.get('cpwd')
-        if upwd and cpwd and upwd != cpwd:
-            raise forms.ValidationError('密碼不相符')
-        return cpwd
+    # def clean_cpwd(self):
+    #     upwd = self.cleaned_data.get('upwd')
+    #     cpwd = self.cleaned_data.get('cpwd')
+    #     if upwd and cpwd and upwd != cpwd:
+
+    #         raise forms.ValidationError('密碼不相符')
+    #     else:
+
+    #         return cpwd
+    
+    # 定义全局的钩子，用来校验密码和确认密码字段是否相同，执行全局钩子的时候，
+    # cleaned_data里面肯定是有了通过前面验证的所有数据
+    
+
+
+    # # 定义全局的钩子，用来校验密码和确认密码字段是否相同，执行全局钩子的时候，cleaned_data里面肯定是有了通过前面验证的所有数据
+    # def clean(self):
+    #     password_value = self.cleaned_data.get('password')
+    #     re_password_value = self.cleaned_data.get('re_password')
+    #     if password_value == re_password_value:
+    #         return self.cleaned_data #全局钩子要返回所有的数据
+    #     else:
+    #         self.add_error('re_password', '两次密码不一致') #在re_password这个字段的错误列表中加上一个错误，并且clean_data里面会自动清除这个re_password的值，所以打印clean_data的时候会看不到它
+    #         raise ValidationError('两次密码不一致')
         
 
 
--- Game_Platform/index/views.py
+++ Game_Platform/index/views.py
@@ -15,87 +15,41 @@ from .froms import *
 
 # Create your views here.
 
-
-
-
-
-
-
 def index_views(request):
     return render(request,'01-index.html')
 
 
-@xframe_options_sameorigin
+# @xframe_options_sameorigin
 def register_views(request):
-#     if request.method == 'POST':
-#         form = RegisterForm(request.POST)
-
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponse('註冊成功')
-#             # return render(request,'02-register.html',locals())
-
-#     else:
-#         form = RegisterForm()
-#         # errorMsg = field.error_messages
-#         # return render(request,'02-register.html',locals())
-
-#     return render(request,'02-register.html',locals())
-
 
     # 判斷get或post請求，得到用戶的請求意圖
     if request.method == 'GET':
         form = RegisterForm()
-
+        
         return render(request,'02-register.html',locals())
     
     else:
         form = RegisterForm(request.POST)
 
         if form.is_valid():
+            
+            # 把cpwd的值刪除，不讓帶進User()數據庫中
+            form.cleaned_data.pop('cpwd')
+
             user = User(**form.cleaned_data)
+
             # 數據庫配置成功並且實體類映射成表之後，該操作可以實現
             user.save() 
+            
+            return HttpResponse('註冊成功')
+
         else:
-
-            # errorMsg = form.errors
-
-            # for field in errorMsg:
-            #     print(field)
-
             return render(request,'02-register.html',locals())
 
-    return render(request,'02-register.html')
-
-        # # 先驗證帳號與信箱是否已存在於數據庫
-        # # 驗證帳號名稱
-        # uname = request.POST['uname']
-        # users = User.objects.filter(uname=uname)
-        # if users:
-        #     # 帳號已存在
-        #     errMsg = '帳號名稱已存在'
-        #     return render(request,'02-register.html',locals())
-        # upwd = request.POST['upwd']
-
-        # ubd = request.POST['ubd']
-
-        # uemail = request.POST['uemail']
-        # uemails = User.objects.filter(uemail=uemail)
-        # if uemails:
-        #     # 信箱已存在
-        #     errMsg = '信箱已註冊過'
-        #     return render(request,'02-register.html',locals())
         
-        # # 接收數據插入到數據庫中
 
-    
-        # user = User()
-        # user.uname = uname
-        # user.upwd = upwd
-        # user.ubd = ubd
-        # user.uemail = uemail
-        # user.save()
-        # return HttpResponse('註冊成功')
+
+
 
 
 
